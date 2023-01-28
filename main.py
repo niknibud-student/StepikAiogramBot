0ed1b9b86671597f510dfd1823186a40517f79e1
Pavel Dubinkin <pdubinkin@yandex.ru>
2023-01-28 19:21:05 +0300

    Проверял как долго длится long polling

--- main.py
+++ main.py
@@ -2,30 +2,25 @@ import requests
 import time
 
 API_URL: str = 'https://api.telegram.org/bot'
-API_CATS_URL: str = 'https://aws.random.cat/meow'
 BOT_TOKEN: str = '5834192413:AAEcAZjHKvXmXCMzngaz9mBpqwVzvImzEr0'
-ERROR_TEXT: str = 'Здесь должна была быть картинка с котиком :('
 
 offset: int = -2
-counter: int = 0
-chat_id: int
-cat_response: requests.Response
-cat_link: str
+timeout: float = 50
+updates: dict
 
-while counter < 100:
-    print('attempt = ', counter)  # Чтобы видеть в консоли что код живёт
-    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset+1}').json()
+
+def do_something() -> None:
+    print('Был апдейт!')
+
+
+while True:
+    start_time = time.time()
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset+1}&timeout={timeout}').json()
 
     if updates['result']:
         for result in updates['result']:
             offset = result['update_id']
-            chat_id = result['message']['from']['id']
-            cat_response = requests.get(API_CATS_URL)
-            if cat_response.status_code == 200:
-                cat_link = cat_response.json()['file']
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendPhoto?chat_id={chat_id}&photo={cat_link}')
-            else:
-                requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={ERROR_TEXT}')
+            do_something()
 
-    time.sleep(1)
-    counter += 1
+    end_time = time.time()
+    print(f'Время между запросами к Telegram Bot API: {end_time - start_time}')

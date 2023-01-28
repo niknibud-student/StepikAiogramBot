830469041ddde992155f12e2a9862c448c344903
Pavel Dubinkin <pdubinkin@yandex.ru>
2023-01-28 18:52:39 +0300

    Научился отправлять запросы к Телеграму и получать апдейты

--- main.py
+++ main.py
@@ -1,10 +1,25 @@
 import requests
+import time
 
-api_url = 'http://numbersapi.com/43'
+API_URL: str = 'https://api.telegram.org/bot'
+BOT_TOKEN: str = '5834192413:AAEcAZjHKvXmXCMzngaz9mBpqwVzvImzEr0'
+TEXT: str = 'Ура! Классный апдейт!'
+MAX_COUNTER: int = 100
 
-response = requests.get(api_url)
+offset: int = -2
+counter: int = 0
+chat_id: int
 
-if response.status_code == 200:
-    print(response.text)
-else:
-    print(response.status_code)
+while counter < MAX_COUNTER:
+
+    print('attempt = ', counter)  # Чтобы видеть в консоли что код живёт
+
+    updates = requests.get(f'{API_URL}{BOT_TOKEN}/getUpdates?offset={offset+1}').json()
+
+    if updates['result']:
+        for result in updates['result']:
+            offset = result['update_id']
+            chat_id = result['message']['from']['id']
+            requests.get(f'{API_URL}{BOT_TOKEN}/sendMessage?chat_id={chat_id}&text={TEXT}')
+    time.sleep(1)
+    counter += 1
